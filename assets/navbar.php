fb1ec6fa2acb8f8c105adb7b83cd6bda26608d70
Kevin Hu <kevin.hu@yale.edu>
2020-05-06 12:35:58 -0400

    adding navbar for testing

---
+++ assets/navbar.php
@@ -0,0 +1,15 @@
+<?php
+echo 
+'<div class="navbar">
+  <a href="/">Home</a>
+  <div class="dropdown">
+    <button class="dropbtn"> About Me &#9660;
+    </button>
+    <div class="dropdown-content">
+			<a href="/about.html">About</a>
+			<a href="/cv.html">CV</a>
+			<a href="/failure_cv.html">Failure CV</a>
+		</div>
+	</div>
+</div>';
+?>
